fa038ba8a27ab69b3948b3912d1de32ac96d2064
jessicasogge <jessica.a.mueller@gmail.com>
2023-09-24 09:09:56 -0500

    Added basic express server and healthcheck route

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ourself-menu-api
+
+This project is an API to provide data to a restaurant menu application
---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 3002;
+const morgan = require('morgan');
+
+app.use(bodyParser.json({}));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(morgan(':method :url :status :res[content-length] :response-time ms'));
+
+app.get('/health-check', (req, res) => res.send('ok'));
+
+app.listen(port, () => console.log(`Express app running on port ${port}!`));
