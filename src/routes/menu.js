da4db44a0c0026860567cff78c5f71b358e1ac33
jessicasogge <jessica.a.mueller@gmail.com>
2023-09-24 19:17:43 -0500

    added entree route

--- src/routes/menu.js
+++ src/routes/menu.js
@@ -13,3 +13,15 @@ router.route('/appetizers')
         return res.status(500).send('Internal Service Error');
     }
 })
+
+router.route('/entrees')
+.get(async (req, res) => {
+    try {
+        const collectionName = "entrees";
+        const result = await get(collectionName);
+        return res.status(200).send(result);
+    } catch (err) {
+        console.log(`Error getting appetizers: ${err.message}`);
+        return res.status(500).send('Internal Service Error');
+    }
+})
