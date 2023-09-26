fed893ebb4257d4501cd129bf1eaa7af1b580b3b
jessicasogge <jessica.a.mueller@gmail.com>
2023-09-26 08:38:18 -0500

    fixed typo

--- src/routes/menu.js
+++ src/routes/menu.js
@@ -9,7 +9,7 @@ router.route('/')
             const result = await get(collectionName);
             return res.status(200).send(result);
         } catch (err) {
-            console.log(`Error getting appetizers: ${err.message}`);
+            console.log(`Error getting menu: ${err.message}`);
             return res.status(500).send('Internal Service Error');
         }
     });
