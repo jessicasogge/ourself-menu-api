91e97fb23db66b3be3e4ef9c0287c09ec547353c
jessicasogge <jessica.a.mueller@gmail.com>
2023-09-25 19:41:52 -0500

    cleanup

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-require('dotenv').config();
 const app = express();
-const port = process.env.PORT || 3002;
+const port = 3002;
 const morgan = require('morgan');
 const db = require('./src/services/dbWrapper.js');
 
 db.getClient();
 
-app.use(bodyParser.json({}));
-app.use(bodyParser.urlencoded({extended: false}));
 app.use(morgan(':method :url :status :res[content-length] :response-time ms'));
 
 app.get('/health-check', (req, res) => res.send('ok'));
